623d48616eadf18a2803784e7ae20b2a7297a339
Dominique Yolin <dominique.yolin@free.fr>
2020-04-10 23:12:10 +0200

    Exemple pour vous aider

--- Automate/Program.cs
+++ Automate/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Automate.Drinks;
+using Automate.Payment;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,24 +10,60 @@ namespace Automate
 {
     class Program
     {
+        static Drink currentDrink;
         static void Main(string[] args)
         {
+            //Permetttre avec une boucle de choisir plusieurs boissons
             Console.WriteLine("Bonjour ! Ça farte ?");
+            selectDrink();
+            PaymentManagement();
+            currentDrink.PreparationDrink();
+            pickUpDrink();
+        }
+
+        static void PaymentManagement()
+        {
+            //Combien ca coute
+            //est ce que vous voulez des suppléments...
+
+            //Comment vous voulez payer etc...
+            receivePayment(null/*on remplace null par la méthode séléctionnée*/);
+        }
+
+        /// <summary>
+        /// @param payment
+        /// </summary>
+        public static void receivePayment(PaymentMethod payment)
+        {
+            // TODO implement here
+        }
+
+        public static void selectDrink()
+        {
             Console.WriteLine("Tu veux quoi pour l'apero ?");
             Console.WriteLine("Tape 1 pour un café, tape 2 pour un thé et tape 3 pour une soupe");
             string userChoice = Console.ReadLine();
             switch (userChoice)
             {
-                case "1": 
+                case "1":
+                    currentDrink = new Coffee();
                     break;
                 case "2":
+                    currentDrink = new Tea();
                     break;
                 case "3":
+                    currentDrink = new Tomato();
                     break;
                 default:
                     Console.WriteLine("Je n'ai pas compris votre demande, réessayez");
                     break;
             }
+           
+        }
+
+        public static void pickUpDrink()
+        {
+            // TODO implement here
         }
     }
 }
